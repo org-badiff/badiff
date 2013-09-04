d659f463bbe4d14bd1bc126b907b956e98ce64d2
robin <robin@robinkirkman.com>
2013-09-04 09:18:33 -0400

    Remove unused imports

--- badiff/src/main/java/org/badiff/io/DiffFile.java
+++ badiff/src/main/java/org/badiff/io/DiffFile.java
@@ -1,7 +1,5 @@
 package org.badiff.io;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
--- badiff/src/main/java/org/badiff/q/OpQueue.java
+++ badiff/src/main/java/org/badiff/q/OpQueue.java
@@ -4,11 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayDeque;
-import java.util.Collection;
 import java.util.Deque;
 import java.util.List;
-import java.util.Queue;
-
 import org.badiff.BADiff;
 import org.badiff.Op;
 
