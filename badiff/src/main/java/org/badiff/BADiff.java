d55871e0d01dd939f35ef24dfd6bbf543600cada
robin <robin@robinkirkman.com>
2013-09-04 06:22:23 -0400

    Rename the apply methods

--- badiff/src/main/java/org/badiff/BADiff.java
+++ badiff/src/main/java/org/badiff/BADiff.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public interface BADiff {
-	public void applyTo(InputStream orig, OutputStream target) throws IOException;
-	public byte[] applyTo(byte[] orig);
+	public void applyDiff(InputStream orig, OutputStream target) throws IOException;
+	public byte[] applyDiff(byte[] orig);
 	public long targetLength();
 }
