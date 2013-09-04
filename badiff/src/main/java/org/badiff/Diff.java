4e0f121a4caf98037c27d4bd465b56639c1e5252
robin <robin@robinkirkman.com>
2013-09-04 08:56:01 -0400

    Utility methods

--- badiff/src/main/java/org/badiff/Diff.java
+++ badiff/src/main/java/org/badiff/Diff.java
@@ -1,5 +1,12 @@
 package org.badiff;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.badiff.io.RuntimeIOException;
+import org.badiff.q.ChukingOpQueue;
+import org.badiff.q.GraphOpQueue;
 import org.badiff.q.OpQueue;
 import org.badiff.q.ReplaceOpQueue;
 
@@ -7,6 +14,23 @@ public class Diff {
 	
 	public static OpQueue queue(byte[] orig, byte[] target) {
 		return new ReplaceOpQueue(orig, target);
+	}
+	
+	public static OpQueue improved(OpQueue q, int chunk) {
+		q = new ChukingOpQueue(q, chunk);
+		q = new GraphOpQueue(q, chunk);
+		return q;
+	}
+	
+	public static byte[] applyDiff(BADiff diff, byte[] orig) {
+		ByteArrayInputStream in = new ByteArrayInputStream(orig);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			diff.applyDiff(in, out);
+		} catch(IOException ioe) {
+			throw new RuntimeIOException(ioe);
+		}
+		return out.toByteArray();
 	}
 
 	private Diff() {
