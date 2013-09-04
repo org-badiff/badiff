d5cfb9a0e5ee0beb745d833ea14ec94904168a1d
robin <robin@robinkirkman.com>
2013-09-04 12:28:09 -0400

    Remove unused imports

--- badiff/src/main/java/org/badiff/q/ParallelGraphOpQueue.java
+++ badiff/src/main/java/org/badiff/q/ParallelGraphOpQueue.java
@@ -3,8 +3,6 @@ package org.badiff.q;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
