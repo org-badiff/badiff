33b6fceab8c01d3b9a4354f4d6235d8f29771911
Robin Kirkman <robin@robinkirkman.com>
2013-09-04 20:24:33 -0400

    PatchUtils utility class

---
+++ badiff/src/main/java/org/badiff/PatchUtils.java
@@ -0,0 +1,38 @@
+package org.badiff;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.badiff.io.EmptyInputStream;
+import org.badiff.io.MemoryDiff;
+
+public class PatchUtils {
+
+	public static PatchOp patchOp(File orig, File target) throws IOException {
+		if(!orig.exists() && !target.exists())
+			return new PatchOp(PatchOp.SKIP, null);
+		if(!target.exists())
+			return new PatchOp(PatchOp.DELETE, null);
+		if(!orig.exists()) {
+			Diff diff = new MemoryDiff();
+			InputStream tin = new FileInputStream(target);
+			diff.store(DiffUtils.queue(new EmptyInputStream(), tin));
+			tin.close();
+			return new PatchOp(PatchOp.CREATE, diff);
+		}
+		
+		Diff diff = new MemoryDiff();
+		InputStream oin = new FileInputStream(orig);
+		InputStream tin = new FileInputStream(target);
+		diff.store(DiffUtils.improved(DiffUtils.queue(oin, tin)));
+		tin.close();
+		oin.close();
+		return new PatchOp(PatchOp.DIFF, diff);
+	}
+	
+	private PatchUtils() {
+	}
+
+}
