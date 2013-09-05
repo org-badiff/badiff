1f2de3a89e2fb2c1ffc519b05d65430a3f522aee
Robin Kirkman <robin@robinkirkman.com>
2013-09-05 05:05:33 -0400

    Remove unrelated methods

--- badiff/src/main/java/org/badiff/ByteArrayDiff.java
+++ badiff/src/main/java/org/badiff/ByteArrayDiff.java
@@ -72,28 +72,4 @@ public class ByteArrayDiff {
 		return Diffs.apply(new UndoOpQueue(md.queue()), target);
 	}
 
-	/**
-	 * Compute and return a patch from the contents of the directory {@code origRoot} to
-	 * the contents of the directory {@code targetRoot}
-	 * @param origRoot
-	 * @param targetRoot
-	 * @return
-	 * @throws IOException
-	 */
-	public byte[] patch(File origRoot, File targetRoot) throws IOException {
-		MemoryPatch mp = new MemoryPatch();
-		mp.store(Patches.patch(origRoot, targetRoot));
-		return Serials.serialize(serial, MemoryPatch.class, mp);
-	}
-	
-	/**
-	 * Apply, in-place, the {@code patch} to the directory {@code origRoot}
-	 * @param origRoot
-	 * @param patch
-	 * @throws IOException
-	 */
-	public void apply(File origRoot, byte[] patch) throws IOException {
-		MemoryPatch mp = Serials.deserialize(serial, MemoryPatch.class, patch);
-		mp.apply(origRoot);
-	}
 }
