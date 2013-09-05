e6750712c43ff5c1446e54b927ee88fd47d082bc
robin <robin@robinkirkman.com>
2013-09-04 21:19:32 -0400

    Remove unused imports

--- badiff/src/main/java/org/badiff/Patches.java
+++ badiff/src/main/java/org/badiff/Patches.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
