88174cb803de40cdd32462de5e088056ed534b63
robin <robin@robinkirkman.com>
2013-09-09 08:29:58 -0400

    CLI for badiff

---
+++ badiff/src/main/java/org/badiff/cli/BadiffCli.java
@@ -0,0 +1,88 @@
+package org.badiff.cli;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.badiff.imp.FileDiff;
+import org.badiff.imp.StreamStoreable;
+import org.badiff.q.OneWayOpQueue;
+import org.badiff.q.OpQueue;
+import org.badiff.q.UndoOpQueue;
+import org.badiff.util.Diffs;
+
+public class BadiffCli {
+
+	public static void main(String[] args) throws Exception {
+		if(args.length == 0) {
+			help();
+			return;
+		}
+		if("diff".equals(args[0])) {
+			if(args.length != 3) {
+				help();
+				return;
+			}
+			FileInputStream orig = new FileInputStream(args[1]);
+			FileInputStream target = new FileInputStream(args[2]);
+			OpQueue q = Diffs.queue(orig, target);
+			q = Diffs.improved(q);
+			new StreamStoreable(System.out).store(q);
+			return;
+		}
+		
+		if("udiff".equals(args[0])) {
+			if(args.length != 3) {
+				help();
+				return;
+			}
+			FileInputStream orig = new FileInputStream(args[1]);
+			FileInputStream target = new FileInputStream(args[2]);
+			OpQueue q = Diffs.queue(orig, target);
+			q = Diffs.improved(q);
+			q = new OneWayOpQueue(q);
+			new StreamStoreable(System.out).store(q);
+			return;
+		}
+		
+		if("patch".equals(args[0])) {
+			if(args.length != 3) {
+				help();
+				return;
+			}
+			FileInputStream orig = new FileInputStream(args[1]);
+			FileDiff diff = new FileDiff(args[2]);
+			diff.apply(orig, System.out);
+			return;
+		}
+
+		if("reverse".equals(args[0])) {
+			if(args.length != 3) {
+				help();
+				return;
+			}
+			FileInputStream patched = new FileInputStream(args[1]);
+			FileDiff diff = new FileDiff(args[2]);
+			OpQueue q = diff.queue();
+			q = new UndoOpQueue(q);
+			q.apply(patched, System.out);
+			return;
+		}
+}
+
+	
+	private static void help() {
+		System.out.println("Command and options required:");
+
+		System.out.println("badiff diff ORIG TARGET");
+		System.out.println("\tWrite a bi-directional diff to standard out");
+		
+		System.out.println("badiff udiff ORIG TARGET");
+		System.out.println("\tWrite a one-way diff to standard out");
+		
+		System.out.println("badiff patch ORIG DIFF");
+		System.out.println("\tWrite the patched file to standard out");
+		
+		System.out.println("badiff reverse PATCHED DIFF");
+		System.out.println("\tWrite the original file to standard out, given a bi-directional diff");
+	}
+}
