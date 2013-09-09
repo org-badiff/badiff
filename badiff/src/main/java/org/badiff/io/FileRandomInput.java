483ed23b946b631f3669baa8aef3f02a72b3027c
robin <robin@robinkirkman.com>
2013-09-09 07:42:52 -0400

    RandomInput for reading a File

---
+++ badiff/src/main/java/org/badiff/io/FileRandomInput.java
@@ -0,0 +1,24 @@
+package org.badiff.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel.MapMode;
+
+public class FileRandomInput extends ByteBufferRandomInput {
+	protected FileInputStream in;
+	
+	public FileRandomInput(File file) throws IOException {
+		this(file, true);
+	}
+	
+	public FileRandomInput(File file, boolean load) throws IOException {
+		super((ByteBuffer) null);
+		in = new FileInputStream(file);
+		super.buf = in.getChannel().map(MapMode.READ_ONLY, 0, file.length());
+		if(load)
+			((MappedByteBuffer) super.buf).load();
+	}
+}
