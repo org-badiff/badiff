1b8975e5a5aa270ae4517dea44c6ba8fc78df37d
Robin Kirkman <robin@robinkirkman.com>
2013-09-11 01:08:49 -0400

    Alter number serialization to optimize "small" instead of optimize positive.  Costs on average a bit per number, but saves about 7 bytes for a small negative.

--- badiff/src/main/java/org/badiff/io/DefaultSerialization.java
+++ badiff/src/main/java/org/badiff/io/DefaultSerialization.java
@@ -208,24 +208,36 @@ public class DefaultSerialization implements Serialization {
 	}
 	
 	public void writeLong(DataOutput out, long val) throws IOException {
+		long v = Math.abs(val) << 1;
+		if(val < 0)
+			v |= 1;
+		writeLongP(out, v);
+	}
+	
+	private void writeLongP(DataOutput out, long val) throws IOException {
 		int v = (int)(val & 0x7f);
 		if((val >>> 7) != 0)
 			v |= 0x80;
 		out.writeByte(v);
 		if((val >>> 7) != 0)
-			writeLong(out, val >>> 7);
+			writeLongP(out, val >>> 7);
 	}
 	
 	public long readLong(DataInput in) throws IOException {
-		return readLong(in, 0, 0);
+		long v = readLongP(in, 0, 0);
+		boolean neg = (v & 1) != 0;
+		long val = v >>> 1;
+		if(neg)
+			val = -val;
+		return val;
 	}
 	
-	private long readLong(DataInput in, long accum, int shift) throws IOException {
+	private long readLongP(DataInput in, long accum, int shift) throws IOException {
 		long b = 0xff & in.readByte();
 		accum |= (b & 0x7f) << (shift);
 		if((b & 0x80) == 0)
 			return accum;
-		return readLong(in, accum, shift + 7);
+		return readLongP(in, accum, shift + 7);
 	}
 	
 	@Override
