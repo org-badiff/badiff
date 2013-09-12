41678bb5e369127f72d943119de876cfc83fa1a9
Robin Kirkman <robin@robinkirkman.com>
2013-09-11 21:08:33 -0400

    CompactingOpQueue combines sequences of Ops with identical operations

---
+++ badiff/src/main/java/org/badiff/q/CompactingOpQueue.java
@@ -0,0 +1,33 @@
+package org.badiff.q;
+
+import java.util.Arrays;
+
+import org.badiff.Op;
+
+public class CompactingOpQueue extends FilterOpQueue {
+
+	public CompactingOpQueue(OpQueue source) {
+		super(source);
+	}
+
+	@Override
+	protected boolean pull() {
+		if(!require(2))
+			return flush();
+
+		while(filtering.get(0).getOp() == filtering.get(1).getOp()) {
+			Op e1 = filtering.remove(0);
+			Op e2 = filtering.remove(0);
+			byte[] data = null;
+			if(e1.getData() != null && e2.getData() != null) {
+				data = Arrays.copyOf(e1.getData(), e1.getData().length + e2.getData().length);
+				System.arraycopy(e2.getData(), 0, data, e1.getData().length, e2.getData().length);
+			}
+			filtering.add(0, new Op(e1.getOp(), e1.getRun() + e2.getRun(), data));
+		}
+		
+		prepare(filtering.remove(0));
+		return true;
+	}
+	
+}
