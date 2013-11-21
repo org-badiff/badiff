0b2dd63bb0054424e7699b6a06fe468e424b5cd3
robin <robin@robinkirkman.com>
2013-11-20 23:33:35 -0500

    Toss in new random data in addition to swapping around old random data.

--- badiff-hadoop/src/main/java/org/badiff/hadoop/aig/InputBytesGenerator.java
+++ badiff-hadoop/src/main/java/org/badiff/hadoop/aig/InputBytesGenerator.java
@@ -25,15 +25,18 @@ public class InputBytesGenerator {
 		for(int i = 0; i < length / 128; i++) {
 			byte[] b1 = new byte[random.nextInt(512)];
 			byte[] b2 = new byte[b1.length];
+			byte[] b3 = new byte[random.nextInt(256)];
 			
 			int pos1 = random.nextInt(to.length - b1.length);
 			int pos2 = random.nextInt(to.length - b2.length);
 			
 			System.arraycopy(to, pos1, b1, 0, b1.length);
 			System.arraycopy(to, pos2, b2, 0, b2.length);
+			random.nextBytes(b3);
 			
 			System.arraycopy(b1, 0, to, pos2, b1.length);
 			System.arraycopy(b2, 0, to, pos1, b2.length);
+			System.arraycopy(b3, 0, to, random.nextInt(to.length - b3.length), b3.length);
 		}
 	}
 	
