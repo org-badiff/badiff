4fb7a1e3ed20b2c448dbfd6cf7c54fdf9b421f6c
robin <robin@robinkirkman.com>
2013-11-25 04:37:12 -0500

    Move BadiffCli out of .cli package

--- badiff/src/main/java/org/badiff/BadiffCli.java
+++ badiff/src/main/java/org/badiff/BadiffCli.java
@@ -27,7 +27,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.badiff.cli;
+package org.badiff;
 
 import java.io.File;
 
