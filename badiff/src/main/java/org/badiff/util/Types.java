426c95a414c4364f29be502fa4ccd2c90cee86c3
robin <robin.kirkman@gmail.com>
2014-10-26 07:00:51 -0400

    Types utility class now uses FlatCollections

--- badiff/src/main/java/org/badiff/util/Types.java
+++ badiff/src/main/java/org/badiff/util/Types.java
@@ -1,7 +1,7 @@
 package org.badiff.util;
 
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public abstract class Types {
@@ -30,51 +30,26 @@ public abstract class Types {
 	};
 
 	private static final int PRIMITIVE_COUNT = PRIMITIVE_TYPES.length;
+
+	private static final Set<Class<?>> WRAPPER_TYPES_SET =
+			new FlatCollections.FlatSet<Class<?>>(WRAPPER_TYPES);
 	
-	private static final Class<?>[] PRIMITIVE_TO_WRAPPER = new Class<?>[moduloHash(PRIMITIVE_TYPES)];
-	private static final Class<?>[] WRAPPER_TO_PRIMITIVE = new Class<?>[moduloHash(WRAPPER_TYPES)];
-	private static final Class<?>[] WRAPPER_TO_WRAPPER = new Class<?>[moduloHash(WRAPPER_TYPES)];
+	private static final Map<Class<?>, Class<?>> PRIMITIVE_TO_WRAPPER =
+			new FlatCollections.FlatMap<Class<?>, Class<?>>(PRIMITIVE_TYPES, WRAPPER_TYPES);
 	
-	static {
-		for(int i = 0; i < PRIMITIVE_COUNT; i++) {
-			Class<?> primitive = PRIMITIVE_TYPES[i];
-			Class<?> wrapper = WRAPPER_TYPES[i];
-			PRIMITIVE_TO_WRAPPER[Math.abs(primitive.hashCode()) % PRIMITIVE_TO_WRAPPER.length] = wrapper;
-			WRAPPER_TO_PRIMITIVE[Math.abs(wrapper.hashCode()) % WRAPPER_TO_PRIMITIVE.length] = primitive;
-			WRAPPER_TO_WRAPPER[Math.abs(wrapper.hashCode()) % WRAPPER_TO_WRAPPER.length] = wrapper;
-		}
-	}
-	
-	private static int moduloHash(Class<?>[] types) {
-		int[] hashCodes = new int[types.length];
-		for(int i = 0; i < types.length; i++)
-			hashCodes[i] = Math.abs(types[i].hashCode());
-		Set<Integer> mods = new HashSet<Integer>();
-		findModulo: for(int modulo = types.length; ; modulo++) {
-			mods.clear();
-			for(int i = 0; i < types.length; i++)
-				if(!mods.add(hashCodes[i] % modulo))
-					continue findModulo;
-			return modulo;
-		}
-	}
+	private static final Map<Class<?>, Class<?>> WRAPPER_TO_PRIMITIVE =
+			new FlatCollections.FlatMap<Class<?>, Class<?>>(WRAPPER_TYPES, PRIMITIVE_TYPES);
 	
 	public static Class<?> toPrimitive(Class<?> type) {
-		if(type.isPrimitive())
+		if(isPrimitive(type))
 			return type;
-		int hc = Math.abs(type.hashCode());
-		if(WRAPPER_TO_WRAPPER[hc % WRAPPER_TO_WRAPPER.length] != type)
-			return null;
-		return WRAPPER_TO_PRIMITIVE[hc % WRAPPER_TO_PRIMITIVE.length];
+		return WRAPPER_TO_PRIMITIVE.get(type);
 	}
 	
 	public static Class<?> toWrapper(Class<?> type) {
-		int hc = Math.abs(type.hashCode());
-		if(type.isPrimitive())
-			return PRIMITIVE_TO_WRAPPER[hc % PRIMITIVE_TO_WRAPPER.length];
-		if(WRAPPER_TO_WRAPPER[hc % WRAPPER_TO_WRAPPER.length] == type)
+		if(isWrapper(type))
 			return type;
-		return null;
+		return PRIMITIVE_TO_WRAPPER.get(type);
 	}
 	
 	public static boolean isPrimitive(Class<?> type) {
@@ -82,16 +57,13 @@ public abstract class Types {
 	}
 	
 	public static boolean isWrapper(Class<?> type) {
-		int hc = Math.abs(type.hashCode());
-		return WRAPPER_TO_WRAPPER[hc % WRAPPER_TO_WRAPPER.length] == type;
+		return WRAPPER_TYPES_SET.contains(type);
 	}
 	
 	public static boolean isPrimitiveOrWrapper(Class<?> type) {
 		return isPrimitive(type) || isWrapper(type);
 	}
 	
-	private Types() {}
-
 	public static Class<?>[] getPrimitiveTypes() {
 		return Arrays.copyOf(PRIMITIVE_TYPES, PRIMITIVE_TYPES.length);
 	}
@@ -103,4 +75,6 @@ public abstract class Types {
 	public static int getPrimitiveCount() {
 		return PRIMITIVE_COUNT;
 	}
+
+	private Types() {}
 }
