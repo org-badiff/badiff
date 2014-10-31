a99551a25f0f251113007202f26f1d3964cd9bd8
robin <robin.kirkman@gmail.com>
2014-10-31 03:24:09 -0400

    Remove unused import

--- badiff-patcher/src/main/java/org/badiff/patcher/client/RepositoryClient.java
+++ badiff-patcher/src/main/java/org/badiff/patcher/client/RepositoryClient.java
@@ -13,7 +13,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 
 import org.apache.commons.io.IOUtils;
