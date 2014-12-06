6f0aa4ec70b52fbd87d7752144794f58345fba51
robin <robin.kirkman@gmail.com>
2014-12-06 04:31:16 -0500

    remove patcher

--- badiff-patcher/src/main/java/org/badiff/patcher/progress/ProgressListener.java
+++
@@ -1,7 +0,0 @@
-package org.badiff.patcher.progress;
-
-import java.util.EventListener;
-
-public interface ProgressListener extends EventListener {
-	public void progressUpdated(ProgressEvent e);
-}
--- badiff-patcher/src/test/java/org/badiff/patcher/client/URLRepositoryClientTest.java
+++
@@ -1,9 +0,0 @@
-package org.badiff.patcher.client;
-
-public class URLRepositoryClientTest extends RepositoryClientTest {
-	@Override
-	public void before() throws Exception {
-		super.before();
-		client = new RepositoryClient(new URLRepositoryAccess(root.toURI().toURL()), storage);
-	}
-}	
