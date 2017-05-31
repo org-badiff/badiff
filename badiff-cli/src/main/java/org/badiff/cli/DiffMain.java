e892699e457ba307269243bf5cb960ddb1058770
Robin Kirkman <robin.kirkman@gmail.com>
2017-05-30 23:40:07 -0400

    Fix compile error

--- badiff-cli/src/main/java/org/badiff/cli/DiffMain.java
+++ badiff-cli/src/main/java/org/badiff/cli/DiffMain.java
@@ -59,7 +59,7 @@ public class DiffMain {
 		
 		Header h = new Header();
 		
-		Header.Optional opt = new Header.Optional();
+		Header.Optional opt = h.new Optional();
 		h.setOptional(opt);
 		opt.setHashAlgorithm(Digests.defaultDigest().getAlgorithm());
 		opt.setPreHash(preHash);
